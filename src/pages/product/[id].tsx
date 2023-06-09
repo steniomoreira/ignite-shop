ae6631fcdd5b74c7a03b00cc7cebb23ec3f768c4
steniomoreira <steniomoreiradev@gmail.com>
2023-06-08 23:48:16 -0300

    feat: SSG fallback

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,10 @@
 
 import { stripe } from "../../lib/stripe";
-import { GetStaticProps } from "next"
+import { GetStaticPaths, GetStaticProps } from "next"
 import Stripe from "stripe";
 import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 interface ProductProps {
     product: {
@@ -15,7 +16,12 @@ interface ProductProps {
     }
 }
 
-export default function Product({ product }: ProductProps) { 
+export default function Product({ product }: ProductProps) {
+    const { isFallback } = useRouter()    
+
+    if (isFallback) {
+        return  'loading...'
+    }
 
     return (
         <ProductContainer>
@@ -32,6 +38,15 @@ export default function Product({ product }: ProductProps) {
             </ProductDetails>
         </ProductContainer>
     )
+}
+
+export const getStaticPaths: GetStaticPaths = async() => {
+    return {
+        paths: [
+            { params: {id: 'prod_NqD4W6hGqTufPD'}}
+        ],
+        fallback: true,
+    }
 }
 
 export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
