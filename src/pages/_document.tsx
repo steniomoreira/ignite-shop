250c13861372633a8a64c6649ad9dfca64bebc7c
steniomoreira <steniomoreiradev@gmail.com>
2023-06-24 20:49:28 -0300

    feat: added favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ export default function Document() {
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
                 <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+                <link rel="icon" href="/icon.svg" type="image/<generated>" sizes="<generated>" />
 
                 <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
             </Head>
